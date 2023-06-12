6558497702eab0bcc873bd4fd31457d64cb80d6c
rishabhg-zs <130342448+rishabhg-zs@users.noreply.github.com>
2023-06-12 22:59:33 +0530

    Update: README.md myNotebook.py

--- myNotebook.py
+++ myNotebook.py
@@ -1,4 +1,8 @@
 # Databricks notebook source
-print("Hii")
+df1 = spark.sql("select * from ngpp_dmp_family_tree_hierarchy")
+display(df1)
 
-print("Hii2")
+# COMMAND ----------
+
+df2 = spark.sql("select * from ngpp_dmp_ppa_act_itm_dtl_tbl")
+display(df2)
